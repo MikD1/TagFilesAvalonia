13ac827c4788f697448634b69ca728dae6a95796
Mikhail Doshevsky <m.doshevsky@gmail.com>
2022-05-01 01:25:00 +0300

    Use JPEG format for preview images

--- TagFiles.Explorer/Models/FilePreviewGenerator.cs
+++ TagFiles.Explorer/Models/FilePreviewGenerator.cs
@@ -12,10 +12,14 @@ public class FilePreviewGenerator
         string? outputFilename = null;
         try
         {
-            outputFilename = Path.Combine(Directory.GetCurrentDirectory(), Guid.NewGuid() + ".png");
+            outputFilename = Path.Combine(Directory.GetCurrentDirectory(), Guid.NewGuid() + ".jpeg");
             string arguments = $@"-i ""{path}"" -vf scale={MaxSize}:-1 -vframes 1 ""{outputFilename}""";
             Process process = Process.Start("ffmpeg", arguments);
             await process.WaitForExitAsync();
+            if (process.ExitCode != 0)
+            {
+                return null;
+            }
 
             byte[] preview = await File.ReadAllBytesAsync(outputFilename);
             return preview;
